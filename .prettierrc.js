d04d6ab8479e79981d8e486e69db7d9223c6ec4c
李明扬 <1342554547@qq.com>
2022-05-05 22:08:26 +0800

    配置添加

---
+++ .prettierrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+  semi: true,
+  tabWidth: 2,
+  useTabs: false,
+  printWidth: 150,
+  singleQuote: false,
+  bracketSpacing: true,
+  jsxBracketSameLine: false,
+  endOfLine: "auto",
+  htmlWhitespaceSensitivity: "css",
+};
